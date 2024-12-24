24a699c851c710c47409d89342ce9e4321c8d752
Anitya Sharma <temp-anitya.sharma@verse.in>
2024-12-24 19:07:37 +0530

    aa

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -5,8 +5,11 @@ import {
   InvoiceForm,
   InvoicesTable,
   LatestInvoiceRaw,
-  Revenue,
+  Revenue
+  
 } from './definitions';
+
+// import {  } from '@/app/lib/definitions';
 import { formatCurrency } from './utils';
 
 export async function fetchRevenue() {
@@ -14,12 +17,12 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
+    console.log('Fetching revenue data...');
     // await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
   } catch (error) {
